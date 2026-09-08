5d9591a5451a3034ff38bd6fa4cfe8685014a856
Asaeluugamer <saritorres1505@gmail.com>
2026-09-08 07:36:47 -0600

    Avance actual

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FieldCentricDrive.java
@@ -0,0 +1,222 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.hardware.gobilda.GoBildaPinpointDriver;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.VoltageSensor;
+
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.Pose2D;
+
+@TeleOp
+
+public class FieldCentricDrive extends OpMode {
+
+    GoBildaPinpointDriver odo;
+
+    private DcMotor FL;
+    private DcMotor FR;
+    private DcMotor BL;
+    private DcMotor BR;
+
+    private DcMotor Shooter;
+    private DcMotor Intake1;
+    private DcMotor Intake2;
+    VoltageSensor Battery;
+
+    @Override
+
+    public void init()
+    {
+        odo = hardwareMap.get(GoBildaPinpointDriver.class, "Odo");
+
+        FL = hardwareMap.get(DcMotor.class, "FL");
+        FR = hardwareMap.get(DcMotor.class, "FR");
+        BL = hardwareMap.get(DcMotor.class, "BL");
+        BR = hardwareMap.get(DcMotor.class, "BR");
+
+        Intake1 = hardwareMap.get(DcMotor.class, "Intake1");
+        Intake2 = hardwareMap.get(DcMotor.class, "Intake2");
+
+        Battery = hardwareMap.voltageSensor.iterator().next();
+
+        FR.setDirection(DcMotorSimple.Direction.REVERSE);
+        FL.setDirection(DcMotorSimple.Direction.REVERSE);
+        BL.setDirection(DcMotorSimple.Direction.FORWARD);
+
+        odo.setOffsets(-84.0, -168.0, DistanceUnit.MM);
+        odo.setEncoderResolution(GoBildaPinpointDriver.GoBildaOdometryPods.goBILDA_4_BAR_POD);
+        odo.setEncoderDirections(GoBildaPinpointDriver.EncoderDirection.FORWARD, GoBildaPinpointDriver.EncoderDirection.FORWARD);
+
+        odo.resetPosAndIMU();
+        Pose2D startPos = new Pose2D(DistanceUnit.MM, -923.925, 1601.14, AngleUnit.RADIANS, 0);
+        odo.setPosition(startPos);
+
+        telemetry.addData("Status", "Initialized");
+        telemetry.addData("X offset", odo.getXOffset(DistanceUnit.MM));
+        telemetry.addData("Y offset", odo.getYOffset(DistanceUnit.MM));
+        telemetry.addData("Device Version Number", odo.getDeviceVersion());
+        telemetry.addData("Device Scalar", odo.getYawScalar());
+        telemetry.update();
+
+    }
+
+    public void move()
+    {
+        double forward = -gamepad1.left_stick_y;
+        double strafe = gamepad1.left_stick_x;
+        double rotate = -gamepad1.right_stick_x;
+
+        Pose2D pose = odo.getPosition();
+        double heading = pose.getHeading(AngleUnit.RADIANS);
+
+        double rot_angle = -heading;
+
+        double cos = Math.cos(rot_angle);
+        double sin = Math.sin(rot_angle);
+
+        double globalStrafe = strafe * cos - forward * sin;
+        double globalForward = strafe * sin + forward * cos;
+
+        double[] newSpeed = new double[4];
+
+        newSpeed[0] = (globalForward - globalStrafe - rotate);
+        newSpeed[1] = (globalForward + globalStrafe + rotate);
+        newSpeed[2] = (globalForward + globalStrafe - rotate);
+        newSpeed[3] = (globalForward - globalStrafe + rotate);
+
+        double max = Math.abs(newSpeed[0]);
+        for (int i = 1; i < newSpeed.length; i++)
+        {
+            if (Math.abs(newSpeed[i]) > max)
+            {
+                max = Math.abs(newSpeed[i]);
+            }
+        }
+
+        if (max > 1.0)
+        {
+            for (int i = 0; i < newSpeed.length; i++)
+            {
+                newSpeed[i] /= max;
+            }
+        }
+
+        FL.setPower(newSpeed[0]);
+        FR.setPower(newSpeed[1]);
+        BL.setPower(newSpeed[2]);
+        BR.setPower(newSpeed[3]);
+
+
+
+        telemetry.addData("Robot X Pos: ", pose.getX(DistanceUnit.MM));
+        telemetry.addData("Robot Y Pos: ", pose.getY(DistanceUnit.MM));
+        telemetry.addData("Robot Heading: ", heading);
+        telemetry.addData("GlobalForward: ", globalForward);
+        telemetry.addData("GlobalStrafe: ", globalStrafe);
+        telemetry.addData("Rotate: ", rotate);
+        telemetry.addData("Speed FL", newSpeed[0]);
+        telemetry.addData("Speed FR", newSpeed[1]);
+        telemetry.addData("Speed BL", newSpeed[2]);
+        telemetry.addData("Speed BR", newSpeed[3]);
+    }
+
+    public void loop()
+    {
+        move();
+
+        /*if (Battery.getVoltage() >= 13)
+        {
+            double SPower = -0.55;
+
+            if (gamepad1.dpad_up)
+            {
+                SPower = -0.7;
+            }
+            else if (gamepad1.dpad_down)
+            {
+                SPower = -0.55;
+            }
+
+            if (gamepad1.right_trigger != 0)
+            {
+                Shooter.setPower(SPower);
+            }
+            else if (gamepad1.left_trigger != 0)
+            {
+                Shooter.setPower(0);
+            }
+        }
+        else if (Battery.getVoltage() < 13)
+        {
+            double SPower = -0.65;
+
+            if (gamepad1.dpad_up)
+            {
+                SPower = -0.8;
+            }
+            else if (gamepad1.dpad_down)
+            {
+                SPower = -0.65;
+            }
+
+            if (gamepad1.right_trigger != 0)
+            {
+                Shooter.setPower(SPower);
+            }
+            else if (gamepad1.left_trigger != 0)
+            {
+                Shooter.setPower(0);
+            }
+        }
+
+         */
+
+        double SPower = -0.8;
+
+        if (gamepad1.dpad_up)
+        {
+            SPower = -0.8;
+        } else if (gamepad1.dpad_down) {
+            SPower = -0.75;
+        }
+
+
+        if (gamepad1.right_bumper)
+        {
+            Intake2.setPower(-1);
+        }
+        else
+        {
+            Intake2.setPower(0);
+        }
+
+        if (gamepad1.a)
+        {
+            Intake1.setPower(-1);
+            Intake2.setPower(1);
+        }
+        else if (gamepad1.b)
+        {
+            Intake1.setPower(1);
+            Intake2.setPower(-1);
+
+        }
+        else
+        {
+            Intake1.setPower(0);
+        }
+
+
+        Pose2D pose = odo.getPosition();
+        telemetry.addData("Robot X: ", odo.getPosX(DistanceUnit.MM));
+        telemetry.addData("Robot Y: ", odo.getPosY(DistanceUnit.MM));
+        telemetry.addData("Robot Angle: ", pose.getHeading(AngleUnit.DEGREES));
+        telemetry.update();
+
+        odo.update();
+    }
+}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Constants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Constants.java
@@ -3,17 +3,54 @@ package org.firstinspires.ftc.teamcode.pedroPathing;
 import com.pedropathing.follower.Follower;
 import com.pedropathing.follower.FollowerConstants;
 import com.pedropathing.ftc.FollowerBuilder;
+import com.pedropathing.ftc.drivetrains.MecanumConstants;
+import com.pedropathing.ftc.localization.constants.PinpointConstants;
 import com.pedropathing.paths.PathConstraints;
+import com.qualcomm.hardware.gobilda.GoBildaPinpointDriver;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
-public class Constants {
-    public static FollowerConstants followerConstants = new FollowerConstants();
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 
+public class Constants {
+    public static FollowerConstants followerConstants = new FollowerConstants()
+            .mass(4.5)
+            .lateralZeroPowerAcceleration(-36.51429266950472)
+            .forwardZeroPowerAcceleration(-29.76573711549381)
+            ;
+
+
+
+    public static MecanumConstants driveConstants = new MecanumConstants()
+            .maxPower(1)
+            .rightFrontMotorName("FR")
+            .rightRearMotorName("BR")
+            .leftRearMotorName("BL")
+            .leftFrontMotorName("FL")
+            .leftFrontMotorDirection(DcMotorSimple.Direction.REVERSE)
+            .leftRearMotorDirection(DcMotorSimple.Direction.REVERSE)
+            .rightFrontMotorDirection(DcMotorSimple.Direction.FORWARD)
+            .rightRearMotorDirection(DcMotorSimple.Direction.FORWARD)
+            .xVelocity(64.9700480784018)
+            .yVelocity(33.05075313538079)
+
+            ;
+
+    public static PinpointConstants localizerConstants = new PinpointConstants()
+            .forwardPodY(6)
+            .strafePodX(6)
+            .distanceUnit(DistanceUnit.INCH)
+            .hardwareMapName("odo")
+            .encoderResolution(GoBildaPinpointDriver.GoBildaOdometryPods.goBILDA_4_BAR_POD)
+            .forwardEncoderDirection(GoBildaPinpointDriver.EncoderDirection.FORWARD)
+            .strafeEncoderDirection(GoBildaPinpointDriver.EncoderDirection.FORWARD);
     public static PathConstraints pathConstraints = new PathConstraints(0.99, 100, 1, 1);
 
     public static Follower createFollower(HardwareMap hardwareMap) {
         return new FollowerBuilder(followerConstants, hardwareMap)
+                .pinpointLocalizer(localizerConstants)
                 .pathConstraints(pathConstraints)
+                .mecanumDrivetrain(driveConstants)
                 .build();
     }
 }
